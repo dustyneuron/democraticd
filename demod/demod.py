08acb3f5c26def0e4024b2fdc1b7feec42d0f54c
Tom Scholl <tom@dustyneuron.com>
2013-08-16 00:02:45 +0100

    Get full pull request info

--- demod/demod.py
+++ demod/demod.py
@@ -4,15 +4,25 @@ import demod.config
     
 class PullRequest:
     def __init__(self, data):
-        self.repo_name = data['repository']['name']
+        self.repo = data['repository']['name']
         self.title = data['subject']['title']
-        self.api_url = data['subject']['url']
+        self.pull_api_url = data['subject']['url']
         self.repo_type = None
+        self.filled = False
         
-    def fill_info(self, data):
-        self.pull_ref = data['head']['ref']
-        self.pull_sha = data['head']['sha']
-        self.pull_clone_url = data['head']['repo']['clone_url']
+    def fill(self, data):
+        if data['base']['repo']['name'] != self.repo:
+            raise Exception('Full pull request name does not match')
+        
+        self.pull_id = data['number']
+        self.description = data['body']
+        self.ref = data['head']['ref']
+        self.sha = data['head']['sha']
+        self.base_ref = data['base']['ref']
+        self.base_sha = data['base']['sha']
+        self.repo_git_url = data['head']['repo']['clone_url']
+        self.repo_api_url = data['head']['repo']['url']
+        self.filled = True
 
 
 
@@ -33,22 +43,32 @@ with open('test/notifications.json') as f:
 
 
 pull_requests = []
+pr_repos = set([])
 for n in notifications:
     if n['subject']['type'] == 'PullRequest':
         pr = PullRequest(n)
-        if pr.repo_name in package_set:
+        if pr.repo in package_set:
             pr.repo_type = 'package'
-        elif pr.repo_name in module_set:
+        elif pr.repo in module_set:
             pr.repo_type = 'module'
         else:
-            print('Ignoring pull request for non-daemon repository "' + pr.repo_name + '"')
+            print('Ignoring pull request for non-daemon repository "' + pr.repo + '"')
             pr = None
             
         if pr:
+            pr_repos.add(pr.repo)
             pull_requests.append(pr)
             
 
-            
+for repo in pr_repos:
+    list_pull_requests = hosted_git.api_list_pull_requests(repo)
+    for pr in pull_requests:
+        if not pr.filled:
+            for full_pr in list_pull_requests:
+                if pr.pull_api_url == full_pr['url']:
+                    pr.fill(full_pr)
+                    break
+                    
 
 
 
--- demod/hostedgit.py
+++ demod/hostedgit.py
@@ -19,19 +19,14 @@ class HostedGit:
         self.base_url = 'https://api.github.com'        
         self.username = username
         self.password = password
-        self.next_api_time = time.time()
+        self.next_notify_time = time.time()
                 
         print('GitHub username/password is ' + username + '/' + password)
         
-    def api_notifications(self):
-        return self.api_call('GET', self.base_url + '/notifications')
-                    
-    def api_call(self, method, url):
-        current_time = time.time()
-        if self.next_api_time > current_time:
-            time.sleep(long(self.next_api_time - current_time))
-    
-        req = urllib.request.Request(url=url, method=method)
+    def _api_call(self, method, url):
+        print('_api_call(' + method + ', ' + url + ')')
+        
+        req = urllib.request.Request(url=self.base_url + url, method=method)
         auth = bytes.decode(base64.b64encode((self.username + ':' + self.password).encode()))
         req.add_header('Authorization', 'Basic %s' % auth)
         req.add_header('User-Agent', 'curl/7.29.0')
@@ -47,23 +42,27 @@ class HostedGit:
         if not result:
             raise HostedGitError('urllib request did not return a result')
         
-        print(result.info())
+        #print(result.info())
+        return result
+        
+    def api_call(self, method, url):
+        result = self._api_call(method, url)
+        return json.loads(result.read().decode('utf-8'))
+
+    def api_notifications(self):
+        current_time = time.time()
+        if self.next_notify_time > current_time:
+            time.sleep(long(self.next_notify_time - current_time))
+    
+        result = self._api_call('GET', '/notifications')
         
         if result.info().get('X-poll-interval'):
             time_to_wait = 2 * float(result.info().get('X-poll-interval'))
-            self.next_api_time = time.time() + time_to_wait
-            print('Will wait ' + str(time_to_wait) + ' secs before next api call')
+            self.next_notify_time = time.time() + time_to_wait
+            print('Will wait ' + str(time_to_wait) + ' secs before next notify call')
         
-        status = int(result.getcode())
-        if (status == 200) or (status == 304):
-            return json.loads(result.read().decode('utf-8'))
-        else:
-            raise HostedGitError('API HTTP error code ' + str(status))
+        return json.loads(result.read().decode('utf-8'))
         
+    def api_list_pull_requests(self, repo):
+        return self.api_call('GET', '/repos/' + self.username + '/' + repo + '/pulls')
         
-        
-    def do_loop(self):
-        while True:
-            # do api poll!
-            data = self.api_call('GET', '/notifications')
-            
