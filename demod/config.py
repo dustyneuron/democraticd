ede94d51053c4604169bbd9e737f358e24414dad
Tom Scholl <tom@dustyneuron.com>
2013-08-15 18:19:13 +0100

    bug fixes

---
+++ demod/config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.3
+
+import os
+import json
+import demod.hostedgit
+
+
+class Config:
+    def __init__(self):
+        self.conf_dir = "/home/tom/.demod/"
+        self.packages_dir = os.path.join(self.conf_dir, "packages")
+        self.json_extension = ".json"
+        
+    def get_package_set(self):
+        package_names = set([])
+        files = os.listdir(self.packages_dir)
+        for f in files:
+            length = len(f) - len(self.json_extension)
+            if (length > 0):
+                package_names.add(f[0:length])
+        return package_names
+        
+    def get_package_data(self, package_name):
+        filename = os.path.join(self.packages_dir, package_name + self.json_extension)
+        with open(filename) as f:
+            data = json.loads(f.read())
+        return data
+
+    def get_package_module_set(self, package_name):
+        return set(self.get_package_data(package_name)['modules'])
+        
+    def get_module_set(self):
+        module_set = set([])
+        package_names = self.get_package_set()
+        for p in package_names:
+            module_set = module_set.union(self.get_package_module_set(p))
+        return module_set
+        
+    def create_hosted_git(self):
+        with open(os.path.join(self.conf_dir, "hosted_git.json")) as f:
+            config = json.loads(f.read())
+        return demod.hostedgit.HostedGit(config['username'], config['password'], config['cache_dir'])
--- demod/hostedgit.py
+++ demod/hostedgit.py
@@ -16,16 +16,16 @@ class HostedGitError(Exception):
 class PullRequest:
     def __init__(self, repo_name, github_data):
         if not github_data.mergeable:
-            raise HostedGitError('Pull request ' + github_data.url
+            raise HostedGitError('Pull request ' + github_data['url']
                 + ' marked not mergeable')
             
         self.repo_name = repo_name
-        self.title = github_data.title
-        self.description = github_data.body
+        self.title = github_data['title']
+        self.description = github_data['body']
         self.pull = {}
-        self.pull.ref = github_data.head.ref
-        self.pull.sha = github_data.head.sha
-        self.pull.clone_url = github_data.head.repo.clone_url
+        self.pull.ref = github_data['head']['ref']
+        self.pull.sha = github_data['head']['sha']
+        self.pull.clone_url = github_data['head']['repo']['clone_url']
 
 
 
@@ -38,6 +38,7 @@ class HostedGit:
         self.next_api_time = time.time()
 
         self.http = httplib2.Http(cache_dir)
+        print('GitHub username/password is ' + username + '/' + password)
         self.http.add_credentials(username, password, 'api.github.com')
         
     def watch_repositories(self, repo_names, callback):
@@ -54,13 +55,15 @@ class HostedGit:
         r, content = self.http.request(url, method=action)
         if not r or not content:
             raise HostedGitError('httplib2 request did not return a request, content tuple')
-            
+        
+        print(r)
+        
         if 'x-poll-interval' in r:
             self.next_api_time = time.time() + float(r['x-poll-interval'])
         
         status = int(r['status'])
         if status != 200 or status != 304:
-            raise HostedGitError('API HTTP error code ' + status)
+            raise HostedGitError('API HTTP error code ' + str(status))
             
         return json.loads(content)
         
