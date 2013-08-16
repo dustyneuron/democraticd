41ba45224f1696e93269bfeaae90f8be913e438f
Tom Scholl <tom@dustyneuron.com>
2013-08-16 20:29:30 +0100

    urllib2 -> urllib3 to re-use https connections

--- demod/hostedgit.py
+++ demod/hostedgit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.3
 
 import json
-import urllib.request
-import urllib.error
+import urllib3
 import base64
 import time
 
@@ -16,51 +15,48 @@ class HostedGitError(Exception):
 
 class HostedGit:
     def __init__(self, username, password):
-        self.base_url = 'https://api.github.com'        
+        self.base_url = 'api.github.com'        
         self.username = username
         self.password = password
         self.next_notify_time = time.time()
         self.last_modified = {}
+        self.conn_pool = None
         
     def _api_call(self, url, method='GET', headers={}):
         print('_api_call(' + method + ', ' + url + ')')
         
         auth = bytes.decode(base64.b64encode((self.username + ':' + self.password).encode()))
         headers['Authorization'] = 'Basic %s' % auth
-        headers['User-Agent'] = 'curl/7.29.0'
+        headers['User-Agent'] = 'democraticd (https://github.com/dustyneuron/democraticd)'
         headers['Accept'] = '*/*'
         last_modified_key = method + ' ' + url
         if last_modified_key in self.last_modified:
             headers['If-Modified-Since'] = self.last_modified[last_modified_key]
-        
-        req = urllib.request.Request(url=self.base_url + url, method=method)
-        for (k, v) in headers.items():
-            req.add_header(k, v)
-        
-        print(req.headers)
+        print(headers)
         print('\n')
+
+        if not self.conn_pool:
+            self.conn_pool = urllib3.HTTPSConnectionPool(self.base_url, maxsize=1)
         
         try:
-            result = urllib.request.urlopen(req)
+            result = self.conn_pool.request(method, url, headers=headers)
         except urllib.error.HTTPError as err:
-            if err.getcode() == 304:
-                result = err
-            else:
-                print(err.info())
-                raise
+            print(err.headers)
+            raise
         if not result:
             raise HostedGitError('urllib request did not return a result')
             
-        if result.info().get('Last-Modified'):
-            self.last_modified[last_modified_key] = result.info().get('Last-Modified')
+        if result.headers.get('last-modified'):
+            self.last_modified[last_modified_key] = result.headers.get('last-modified')
         
-        print(result.info())
+        print(result.headers)
         return result
         
-    def _get_json(self, result):
-        if result.getcode() == 304:
+    def _return_json(self, result):
+        if result.status == 304:
             return []
-        data = json.loads(result.read().decode('utf-8'))
+        data = json.loads(result.data.decode('utf-8'))
+        result.release_conn()
         if len(data) >= 30:
             raise HostedGitError('TODO: implement pagination')
         return data
@@ -74,13 +70,13 @@ class HostedGit:
             
         result = self._api_call('/notifications')
                 
-        if result.info().get('X-poll-interval'):
-            time_to_wait = float(result.info().get('X-poll-interval'))
+        if result.headers.get('x-poll-interval'):
+            time_to_wait = float(result.headers.get('x-poll-interval'))
             self.next_notify_time = time.time() + time_to_wait
         
-        return self._get_json(result)
+        return self._return_json(result)
         
     def api_list_pull_requests(self, repo):
         result = self._api_call('/repos/' + self.username + '/' + repo + '/pulls')
-        return self._get_json(result)
+        return self._return_json(result)
         
