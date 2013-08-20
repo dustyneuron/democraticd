deefe1da638fbb9c4e02b2ab0fa7f1409dcf55ca
Tom Scholl <tom@dustyneuron.com>
2013-08-20 17:49:48 +0100

    Nicer main class containing shared data

--- demod/daemon.py
+++ demod/daemon.py
@@ -5,70 +5,78 @@ import gevent
 import gevent.server
 import gevent.event
     
-def do_github_actions(config, hosted_git):
-    print('At top of daemon loop')
-    repo_dict = {}
-    for repo in config.get_repo_set():
-        print('Reading saved pull requests for "' + str(repo) + '"')
-        repo_dict[repo] = config.read_pull_requests(repo, pullrequest.PullRequest)
+class DemoDaemon:
+    def __init__(self):
+        self.quit_event = gevent.event.Event()
+        self.config = demod.config.Config()
+        self.hosted_git = self.config.create_hosted_git(self.quit_event)
 
-    print('Getting new pull request notifications from GitHub API')
-    new_repo_dict = pullrequest.get_new_pull_requests(config, hosted_git)
-    if new_repo_dict:
-        for repo in repo_dict.keys():
-            if repo in new_repo_dict:
-                repo_dict[repo] = pullrequest.update_pull_request_list(repo_dict[repo], new_repo_dict[repo])
-            
-    print('Commenting on pull requests')
-    pullrequest.comment_on_pull_requests(hosted_git, repo_dict)
-    
-    for (repo, pr_list) in repo_dict.items():
-        print('Saving pull requests for "' + str(repo) + '"')
-        config.write_pull_requests(repo, pr_list)
+        self.server = gevent.server.StreamServer(('localhost', 9999), lambda sock, addr: DemoDaemon.command_server(self, sock, addr))
         
+    def start(self):
+        print('Starting server on port 9999')
+        self.server.start()
+        
+        while not self.quit_event.is_set():
+            self.do_github_actions()
 
-def command_server(socket, address):
-    print ('New connection from %s:%s' % address)
-    socket.sendall('Welcome to the Democratic Daemon server!\r\n')
-    help_message = 'Commands are "quit", "list" and "approve"\r\n'
-    socket.sendall(help_message)
-    fileobj = socket.makefile()
-    while True:
-        line = fileobj.readline()
-        if not line:
-            print ("client disconnected")
-            break
+
+    def do_github_actions(self):
+        print('At top of daemon loop')
+        repo_dict = {}
+        for repo in self.config.get_repo_set():
+            print('Reading saved pull requests for "' + str(repo) + '"')
+            repo_dict[repo] = self.config.read_pull_requests(repo, pullrequest.PullRequest)
+
+        print('Getting new pull request notifications from GitHub API')
+        new_repo_dict = pullrequest.get_new_pull_requests(self.config, self.hosted_git)
+        if new_repo_dict:
+            for repo in repo_dict.keys():
+                if repo in new_repo_dict:
+                    repo_dict[repo] = pullrequest.update_pull_request_list(repo_dict[repo], new_repo_dict[repo])
+                
+        print('Commenting on pull requests')
+        pullrequest.comment_on_pull_requests(self.hosted_git, repo_dict)
+        
+        for (repo, pr_list) in repo_dict.items():
+            print('Saving pull requests for "' + str(repo) + '"')
+            self.config.write_pull_requests(repo, pr_list)
             
-        command = line.decode().strip().lower()
-        if command == 'quit':
-            print ("client told server to quit")
-            quit_event.set()
-            server.stop()
-            break
-        elif command == 'list':
-            pass
-        elif command == 'approve':
-            pass
-        else:
-            fileobj.write(('Unknown command "' + command + '"\r\n').encode())
-            fileobj.write(help_message.encode())
             
-        fileobj.write(line)
-        fileobj.flush()
-        print ("echoed back %r" % line)
+    def command_server(self, socket, address):
+        print ('New connection from %s:%s' % address)
+        socket.sendall('Welcome to the Democratic Daemon server!\r\n')
+        help_message = 'Commands are "quit", "list" and "approve"\r\n'
+        socket.sendall(help_message)
+        fileobj = socket.makefile()
+        while True:
+            line = fileobj.readline()
+            if not line:
+                print ("client disconnected")
+                break
+                
+            command = line.decode().strip().lower()
+            if command == 'quit':
+                print ("client told server to quit")
+                self.quit_event.set()
+                self.server.stop()
+                break
+            elif command == 'list':
+                pass
+            elif command == 'approve':
+                pass
+            else:
+                fileobj.write(('Unknown command "' + command + '"\r\n').encode())
+                fileobj.write(help_message.encode())
+                
+            fileobj.write(line)
+            fileobj.flush()
+            print ("echoed back %r" % line)
+
 
 
 if __name__ == '__main__':
-    quit_event = gevent.event.Event()
-    server = gevent.server.StreamServer(('localhost', 9999), command_server)
-    print('Starting server on port 9999')
-    server.start()
-    
-    config = demod.config.Config()
-    hosted_git = config.create_hosted_git(quit_event)
-    
-    while not quit_event.is_set():
-        do_github_actions(config, hosted_git)
+    DemoDaemon().start()
 
 
 # MVP:
