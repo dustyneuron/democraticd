a2bc6591af414b2dd82b2328f2daf6ecbcbdb3b6
Tom Scholl <tom@dustyneuron.com>
2013-08-20 16:14:31 +0100

    Gevent echo server - doesn't work yet

--- demod/daemon.py
+++ demod/daemon.py
@@ -1,5 +1,6 @@
 import demod.config
 from . import pullrequest
+
     
 def daemon_loop(config, hosted_git):
     while True:
@@ -28,6 +29,37 @@ def go():
     config = demod.config.Config()
     hosted_git = config.create_hosted_git()
     daemon_loop(config, hosted_git)
+
+
+
+
+from gevent.server import StreamServer
+
+def echo(socket, address):
+    print('New connection from %s:%s' % address)
+    fileobj = socket.makefile('w')
+    fileobj.write('Welcome to the echo server! Type quit to exit.\r\n')
+    fileobj.flush()
+    while True:
+        line = fileobj.readline()
+        if not line:
+            print('client disconnected')
+            break
+        if line.strip().lower() == 'quit':
+            print('client quit')
+            break
+        fileobj.write(line)
+        fileobj.flush()
+        print('echoed ' + repr(line))
+
+
+
+server = StreamServer(('localhost', 9999), echo)
+# to start the server asynchronously, use its start() method;
+# we use blocking serve_forever() here because we have no other jobs
+print('Starting echo server on port 9999')
+server.serve_forever()
+
 
 
 # MVP:
