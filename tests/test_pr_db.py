209b54108ca22d3ff7337ac99db2220de7ecb5ba
Tom Scholl <tom@dustyneuron.com>
2013-08-30 17:19:43 +0100

    Tests

--- tests/test_pr_db.py
+++ tests/test_pr_db.py
@@ -4,7 +4,7 @@ import democraticd.pr_db
 import democraticd.config
 from democraticd.utils import DebugLevel
 
-class TestSequenceFunctions(unittest.TestCase):
+class TestPrDbi(unittest.TestCase):
 
     def setUp(self):
         self.config = democraticd.config.Config(debug_level=DebugLevel.DEBUG, mark_read=False)
@@ -13,7 +13,6 @@ class TestSequenceFunctions(unittest.TestCase):
     def test_foo(self):
         self.assertEqual(1, 1)
         self.assertTrue(True)
-        self.assertRaises(Exception, nonexistant_func, (1,2,3))
 
 if __name__ == '__main__':
     unittest.main()
